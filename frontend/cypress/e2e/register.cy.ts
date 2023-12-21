22962c833ee5a1fe53b7ced0bd7931c9b9ebf69f
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-12-21 10:56:38 -0800

    test register user

---
+++ frontend/cypress/e2e/register.cy.ts
@@ -0,0 +1,40 @@
+/// <reference types = "cypress" />
+
+describe("Reisters a user", () => {
+    it ("passes", () => {
+        cy.visit('http://localhost:4200/register')
+
+        cy.get('[data-cy="username"]').type('Sanny')
+        cy.get('[data-cy="email"]').type('emmanuel@gmail.com')
+        cy.get('[data-cy="fullname"]').type('Emmanuel Kipsang')
+        cy.get('[data-cy="password"]').type('@Emmanuel123')
+        cy.get('[data-cy="confirmpassword"]').type('@Emmanuel123')
+
+        
+        cy.get('[data-cy="registerBtn"]').click()
+
+        
+
+
+    })
+})
+
+
+
+
+
+// describe("Registers a user", () => {
+
+//     it("passes", () => {
+//       cy.visit('http://localhost:4200/register')
+  
+//       cy.get('[data-cy="username"]').type('Emmanuel kipsang')
+//       cy.get('[data-cy="email"]').type('emmanuel@gmail.com')
+//       cy.get('[data-cy="password"]').type('@Emmanuel123')
+      
+//       cy.get('[data-cy="registerBtn"]').click()
+  
+      
+//     })
+  
+//   })
